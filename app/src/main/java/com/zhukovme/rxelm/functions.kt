4116f2bc4df03c7e5c050cabbdfc5eb92b405cc5
zhukovme <zhukovme@gmail.com>
2019-06-19 00:25:51 +0300

    Add asStatelessEffect extension for Completable

--- app/src/main/java/com/zhukovme/rxelm/functions.kt
+++ app/src/main/java/com/zhukovme/rxelm/functions.kt
@@ -3,10 +3,12 @@ package com.zhukovme.rxelm
 import com.zhukovme.rxelm.program.BatchCmd
 import com.zhukovme.rxelm.program.Cmd
 import com.zhukovme.rxelm.program.Idle
-import com.zhukovme.rxelm.program.Msg
+import io.reactivex.Completable
 import io.reactivex.Single
 
-inline fun statelessEffect(crossinline operations: () -> Unit): Single<Msg> {
+fun Completable.asStatelessEffect(): Single<Idle> = this.andThen(Single.just(Idle))
+
+inline fun statelessEffect(crossinline operations: () -> Unit): Single<Idle> {
     return Single.fromCallable {
         operations()
     }.map { Idle }
